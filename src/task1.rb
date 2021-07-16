3444de81ae000ad65a844ff8f903b2460c5bebae
VikaBielik <86297454+VikaBielik@users.noreply.github.com>
2021-07-16 15:32:24 +0300

    Update task1.rb

--- src/task1.rb
+++ src/task1.rb
@@ -1,15 +1,18 @@
-def checkered_board(d, s, sumbol)
-    arr =[ ]
-    for i in 0...d
-      for j in 0...s
-        if (i + j) % 2 ==0
-          arr << (' ')
-        end
-      end
+def chessboard(l, w , s)
+arr = []
 
-    def star (d, s, sumbol)
-      str = chess(d, s, symbol)
-      return str.join 
-    end
-    print star [6, 6, '*']
+line1 = (s + ' ') * (w / 2)
+line1 << s if w.odd?
+line2 = ' ' + line1.chomp(line1[-1])
+
+for  i in 1.. (l / 2)
+    answer << line1 + "\n"
+    answer << line2 + "\n"
+end
+
+arr << line1 if l.odd?
+
+return arr
+end
+
     
