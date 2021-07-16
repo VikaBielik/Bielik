948963da2c37a52e692ad141a42bbbdeda1b47d4
VikaBielik <86297454+VikaBielik@users.noreply.github.com>
2021-07-16 22:12:33 +0300

    Update task1.rb

--- src/task1.rb
+++ src/task1.rb
@@ -1,18 +1,1 @@
-def chessboard(l, w , s)
-arr = ' '
 
-line1 = (s + ' ') * (w / 2)
-line1 << s if w.odd?
-line2 = ' ' + line1.chomp(line1[-1])
-
-for  i in 1.. (l / 2)
-    answer << line1 + "\n"
-    answer << line2 + "\n"
-end
-
-arr << line1 if l.odd?
-
-return arr
-end
-
-    
