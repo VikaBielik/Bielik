431a8fff9c9780f09173a48ef2358c598638ce4f
VikaBielik <86297454+VikaBielik@users.noreply.github.com>
2021-07-19 01:51:01 +0300

    Update task6.rb

--- src/task6.rb
+++ src/task6.rb
@@ -1,27 +1,27 @@
-#входные парамеры: длина и значение минимального квадрата(ЗМК)
-#вывод: строка с рядом чисел
-#(3, 9) #’3, 4, 5’
-#(5, 30) #’6, 7, 8, 9, 10’
-#(1, 1) #’1’
-#(5, 0) #’1, 2, 3, 4, 5’
+# входные парамеры: длина и значение минимального квадрата(ЗМК)
+# вывод: строка с рядом чисел
+# вход: длина и значения минимального квадрата - целые числа
+#(3, 9)   #’3, 4, 5’
+#(5, 30)  # ’6, 7, 8, 9, 10’
+#(1, 1)   # ’1’
+#(5, 0)   # ’0, 1, 2, 3, 4’
 #(0, 234) # ‘’
 
 
-def squarenum(count, maxsquare)
+def squarenum(d, minsquare)
     ar = []
-    a = Math.sqrt(maxsquare).ceil
-    if a.kind_of?Integer
+    a = Math.sqrt(minsquare).ceil
+    if a.is_a?Integer
         a = a
     else
         a = (a + 1).round
-        
     end
-    for i in a..a + count - 1
+    for i in a..a + d - 1
         ar.push(i)
     end
 
 return ar
 end
 
-print squarenum(count 5, maxsquare 30)
+p squarenum(5, 0)
 
