d707b4aafaa0a3953728efabdc0f142babc54810
VikaBielik <86297454+VikaBielik@users.noreply.github.com>
2021-07-19 15:56:58 +0300

    Update task1.rb

--- src/task1.rb
+++ src/task1.rb
@@ -1,23 +1,20 @@
 # l - длина(количество строк по вертикали)
 # w - ширина(количество символов по вертикали)
 # s - строка длиной 1 символ 
-
 def chessboard(l, w, s)
-    a = []
-    str1 = (s + ' ') * (w/2)
-    str1 << s if w.odd?
-    str2 = ' ' + str1.chomp(str1[-1])
-    for  i in 1.. (l / 2)
-        a << str1 + "\n"
-        a << str2 + "\n"
+ a =[]
+ for i in 0...l 
+    for j in 0...w 
+        if (i + j)/2 == 0
+            arr << (' ')
+        end
     end
-    a << str1 if l.odd?
-    return a
 end
 
-def play(l, w, s)
-    str = chessboard(l, w, s)
+def start(l, w, s)
+    str = chessboard(l,w,s)
     return str.join
 end
-print play(4, 12, "*")
+end
+print start(12,4, '*')
 
